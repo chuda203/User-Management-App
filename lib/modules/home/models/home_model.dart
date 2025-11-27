102ef404d7567abb20c985bcabe9727e3e440a41
chuda203 <hudakhoir203@gmail.com>
2025-11-28 05:19:31 +0700

    refactor(home,user_list,user_detail): cleanup empty model files

--- lib/modules/home/models/home_model.dart
+++
@@ -1,2 +0,0 @@
-// Home module doesn't need specific models since it uses the core User model
-// This file exists to comply with the directory structure requirement
--- lib/modules/user_list/models/user_list_model.dart
+++
@@ -1,2 +0,0 @@
-// User list module doesn't need specific models since it uses the core User model
-// This file exists to comply with the directory structure requirement
